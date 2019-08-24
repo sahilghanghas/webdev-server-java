71e235bd2bbe73914e0a547ef6eb09c944105deb
sahil <sahilghanghas93@gmail.com>
2019-08-23 18:40:16 -0700

    userservice updated

--- src/main/java/com/example/webdevserverjava/model/User.java
+++ src/main/java/com/example/webdevserverjava/model/User.java
@@ -1,5 +1,47 @@
 package com.example.webdevserverjava.model;
 
 public class User {
-
+	private Integer id;
+	private String uername;
+	private String password;
+	private String firstName;
+	private String lastName;
+	
+	public User() {}
+	public User(int id, String username, String firstName, String lastName) {
+		this.id = id;
+		this.firstName = firstName;
+		this.uername = username;
+		this.lastName = lastName;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUername() {
+		return uername;
+	}
+	public void setUername(String uername) {
+		this.uername = uername;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 }
--- src/main/java/com/example/webdevserverjava/services/UserService.java
+++ src/main/java/com/example/webdevserverjava/services/UserService.java
@@ -1,5 +1,45 @@
 package com.example.webdevserverjava.services;
 
-public class UserService {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.webdevserverjava.model.User;
+
+@RestController
+public class UserService {
+	User alice = new User(123, "alice", "Alice", "Wonderland");
+	User bob = new User(234, "bob", "Bob", "Marley");
+	User[] users = {alice,bob};
+	
+	@GetMapping("/api/user")
+	public User[] findAllUser() {
+		return users;
+	}
+	
+	@GetMapping("/api/user/{userId}")
+	public User findUserById(
+			@PathVariable("userId") Integer id) {
+		for(User user: users) {
+			if(id == user.getId().intValue())
+				return user;
+		}
+		return null;
+	}
+	
+//	@PostMapping("/api/user")
+//	public User createUser(
+//			@RequestBody User user) {
+//		return newUser;
+//	}
+	
+//	public void deleteUser(Integer id) {
+//		
+//	}
+//	
+//	public User updateUser(Integer id, User user) {
+//		
+//	}
 }
